e6f8ea19e97896f6ca87deede218f676e56e9290
DiskiNir <nid5xq@virginia.edu>
2018-07-18 14:21:48 -0400

    Correction to Silly Mistake

--- Diffusion.m
+++ Diffusion.m
@@ -4,7 +4,7 @@
 
 %Diffusion Module
 %Input vector "M" is an array of w by h 
-%M(x,y): is [a square with a [AI2] at x-y coordinate of the medium
+%M(x,y): is [a rectangle with a [AI2] at x-y coordinate of the medium
 %
 %Function will calculate diffusion dynamics of the medium
 %Function will compute change d[AI2]/dt for each Mxy
