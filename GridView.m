63eb511a0055da929f4f28f5a22ac1bd20b01903
DiskiNir <nid5xq@virginia.edu>
2018-07-24 17:24:39 -0400

    GridView Ver 1.0

--- GridView.m
+++ GridView.m
@@ -1,33 +1,74 @@
-function [ X ] = GridView(M,PSI)
-%Creates a grid based on various elements of PSI
+function [ X ] = GridView(M,PSI,PSIIndex)
+%Creates a grid based on various elements of PSI or M
+%PSIIndex references ROW in PSI matrix
 close all
 
-MSIZE =size(M);
-MH = MSIZE(1);
-MW = MSIZE(2);
-
-PSISIZE =size(PSI);
-PH = PSISIZE(1);
-PW = PSISIZE(2);
-
-GRID = zeros(MH,MW);
-
-
-for Mcounter = 1:MH*MW
-    GRID(Mcounter) = M(Mcounter);
+if PSIIndex == 0
+    
+    %Determine Dim of Matrix M
+    MSIZE =size(M);
+    MH = MSIZE(1);
+    MW = MSIZE(2);
+    GRID = zeros(MH,MW);
+    
+    %Determine number of Cells
+    PSISIZE =size(PSI);
+    numofcells = PSISIZE(2);
+    
+    %Change value of grid to corresponding value in Matrix M.
+    for Mcounter = 1:MH*MW
+        GRID(Mcounter) = M(Mcounter);
+    end
+    
+    figure(1);
+    imagesc(GRID);
+    axis image;
+    colormap parula;
+    colorbar;
+    hold on;
+    
+    for Cellcounter = 1:numofcells
+        plot(PSI(1,Cellcounter),PSI(2,Cellcounter), 's')
+    end
+    
+else
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    
+    %Determine number of Cells
+    PSISIZE =size(PSI);
+    numofcells = PSISIZE(2);
+    H = 100;
+    W = 100;
+    marksize = 3;
+    GRID = zeros(H,W);
+    BooleanGrid = zeros(H,W);
+    
+    %Change value of grid to corresponding value in Matrix M.
+    for Cellcounter = 1:numofcells
+        GRID(PSI(1,Cellcounter),PSI(2,Cellcounter)) = PSI(PSIIndex,Cellcounter);
+        BooleanGrid(PSI(1,Cellcounter),PSI(2,Cellcounter)) = 1;
+    end
+    
+    figure(1);
+    imagesc(GRID);
+    axis image;
+    colormap parula;
+    colorbar;
+    hold on;
+    
+    for Hcounter = 1:H
+        for Wcounter = 1:W
+            if BooleanGrid(Hcounter,Wcounter) == 0
+                plot(Wcounter,Hcounter,'x',...
+                        'MarkerSize',marksize,...
+                        'MarkerEdgeColor','w',...
+                        'MarkerFaceColor',[1,1,1]);
+            end
+        end
+    end
 end
-
-% for PSICounter = 1:PH*PW
-%     plot(PSI(1,counter),PSI(2,counter), '-o')
-% end
-figure(1);
-imagesc(GRID);
-axis image;
-
-colormap winter;
-colorbar;
-
 % PSI = [ 1 3 5 7; 1 2 4 9; 4 5 9 2]
-% M = [ 1 3 5 7; 1 2 4 9; 4 5 9 2; 0 2 3 9]
+
+%M = rand(100)*10;'
 end
 
