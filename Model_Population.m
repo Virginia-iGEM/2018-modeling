4ad59b3a15086fb9032e306370b91526cae8328b
rytaylor <rmt2dt@virginia.edu>
2018-07-26 17:26:20 -0400

    Changes to Model_Population

--- Model_Population.m
+++ Model_Population.m
@@ -1,61 +1,95 @@
 %{
 Virginia iGEM 2018
-Population Model
+Population Model of Quorum Sensing
 Requires:
     Cellular_Function.m
     Diffusion.m
     Structure.m
+    GridView.m
 
 Manipulate Psi and M matricies to test effects of initial conditions:
 Manipulate constants within Cellular_Function to test sensitivity
 
 %}
+%Imports
+import Structure.*
+import GridView.*
+%----------------------
 
-%%% Cell Matrix %%%
-n = 9;           % Number of Cells (needs to be square numer)
-m = 4;              % Number of Parameters for each Cell
+%Model Parameters
+parameters = containers.Map;
+parameters('n') = 9;                % Number of Cells (needs to be square numer)
+parameters('m') = 2+4;              % Number of Parameters for each Cell
+parameter('w') = n;               % Medium/Diffusion Grid Width
+parameters('h') = n;              % Medium/Diffusion Grid height
+parameters('t_i') = 0;              % Set initial time to 0
+parameters('t_f') = 10;             % Final time
+parameters('dt')= 0.1;              % Constant timestep 
+parameters('D') = 10^-10;           % Diffusion coefficient
+parmeters('index') = 0;
+%--------------------------------------------------------
 
-%%% Diffusion Matrix %%%
-w = 100;            % Medium/Diffusion Grid Width
-h = 100;            % Medium/Diffusion Grid height
+%Configuration Parameters
+config = containers.Map;
+config('workers') = 4; 
 
-%%% Constants %%%
+config('print') = 1;% Should progress reports be printed to console?
+config('n_prints') = 4;% How many times should we print progress reports?
 
-t_i = 0;            % Set initial time to 0
-t_f = 10;          % Final time
-dt = 0.1;             % Constant timestep 
-D = 10^-10;         % Diffusion coefficient
+config('write') = 0; % Should .csv files be written to the data folder?
+config('n_writes') = 50;  % How many times should we write .csv files?
 
-gm = gmdistribution(1,0.05);
+config('Psi_x') = 1;    % The row of Psi which contains the x positions of cells
+config('Psi_y') = 2;    % The row of Psi which contains the y position of cells
+config('Psi_Ai') = 4;   % The row of Psi which contains the A_o value for cells
+config('Psi_Ao') = 5;   % The row of Psi which contains the A_i value for cells
+%----------------------------------------------------
 
+%Randomization Distribution
+gm = gmdistribution(1,0.000005);
+%------------------------------
+
+%Initial Matrices
 Psi = zeros(m,n);
 M = zeros(w,h);
+%------------------------------
 
-%initial c vector---------
-c_i = zero(1,m)
-c_i(3) = 0; %Ap
-c_i(4) = 0; %Ai
-c_i(5) = 10; %Ao
-c_i(6) = 1; %B
+%initial c vector
+c_i = zeros(m,1);
+c_i(3,1) = 0; %Ap
+c_i(4,1) = 0; %Ai
+c_i(5,1) = 10; %Ao
+c_i(6,1) = 1; %B
 %-------------------------
 
 %initialize Psi Matrix
-for x = (floor((sqrt(n)-w)/2)):(floor((sqrt(n)+w)/2))
-    for y = (floor((sqrt(n)-h)/2)):(floor((sqrt(n)+h)/2))
+i = 1;
+for x = floor(w/2-sqrt(n)/2):1:floor(w/2+sqrt(n)/2)
+    for y = floor(h/2-sqrt(n)/2):1:floor(h/2+sqrt(n)/2)
         if i<=n
-            Psi(1,i) = x;
-            Psi(2,i) = y;
+            Psi(1,i) = round(x);
+            Psi(2,i) = round(y);
             i = i+1;
         end
     end
 end
 
 for i = 1:n
-    for j = 2:m
-    Psi(j,i) = c_i(j)*random(gm);
+    for j = 3:m
+    Psi(j,i) = c_i(j,1)*random(gm);
     end
 end
 %--------------------------
-    
-    
 
+%Simulate
+[Psi_cells, M_cells] = SimuPop(Psi, M, parameters, config);
+%-----------------
+
+%Statistically Analyze
+Psi
+M
+%Graph
+
+%View
+GridView(M_cells,Psi_cells,parameters(index))
+%----------------
--- Structure.m
+++ Structure.m
@@ -2,30 +2,27 @@
 % Expected Predefined Functions %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% Diffusion(M, D, dt)
-% Function(c)
+import Diffusion.m
+import Cellular_Function.m
 
-%%%%%%%%%%%%%%
-% Parameters %
-%%%%%%%%%%%%%%
 
 % Simulation %
 
-variables = containers.Map;
+parameters = containers.Map;
 %%% Cell Matrix %%%
-variables('n') = 1000;           % Number of Cells
-variables('m') = 4;              % Number of Parameters for each Cell
+parameters('n') = 1000;           % Number of Cells
+parameters('m') = 4;              % Number of Parameters for each Cell
 
 %%% Diffusion Matrix %%%
-variables('w') = 1000;            % Medium/Diffusion Grid Width
-variables('h') = 1000;            % Medium/Diffusion Grid height
+parameters('w') = 1000;            % Medium/Diffusion Grid Width
+parameters('h') = 1000;            % Medium/Diffusion Grid height
 
 %%% Constants %%%
 
-variables('t_i') = 0;            % Set initial time to 0
-variables('t_f') = 10;          % Final time
-variables('dt') = 0.1;             % Constant timestep 
-variables('D') = 10^-10;         % Diffusion coefficient
+parameters('t_i') = 0;            % Set initial time to 0
+parameters('t_f') = 10;          % Final time
+parameters('dt') = 0.1;             % Constant timestep 
+parameters('D') = 10^-10;         % Diffusion coefficient
 
 % Settings %
 
@@ -49,20 +46,20 @@ config('write') = 0;
 % How many times should we write .csv files?
 config('n_writes') = 50;
 
-% Which rows are the different variables in the cell matrix?o
+% Which rows are the different parameters in the cell matrix?o
 config('Psi_x') = 1;    % The row of Psi which contains the x positions of cells
 config('Psi_y') = 2;    % The row of Psi which contains the y position of cells
-config('Psi_Ai') = 3;   % The row of Psi which contains the A_o value for cells
-config('Psi_Ao') = 4;   % The row of Psi which contains the A_i value for cells
+config('Psi_Ai') = 4;   % The row of Psi which contains the A_o value for cells
+config('Psi_Ao') = 5;   % The row of Psi which contains the A_i value for cells
 
 %%%%%%%%%%%%%%%%%
 % Initial Setup %
 %%%%%%%%%%%%%%%%%
 
 % Temporary setup function
-function [Psi, M] = setup(variables, config)
-    Psi = ones(variables('m'), variables('n'));   % Matrix of cell column vectors with m rows and n columns
-    M = zeros(variables('w'), variables('h'));     % w x h discrete diffusion matrix
+function [Psi, M] = setup(parameters, config)
+    Psi = ones(parameters('m'), parameters('n'));   % Matrix of cell column vectors with m rows and n columns
+    M = zeros(parameters('w'), parameters('h'));     % w x h discrete diffusion matrix
 
     % Assign all A_0 in the cell to the initial value in M
     for col=1:size(Psi, 2)
@@ -74,11 +71,11 @@ end
 
 
 
-function [Psi_snapshots, M_snapshots] = simulate(Psi, M, variables, config)
+function [Psi_snapshots, M_snapshots] = SimuPop(Psi, M, parameters, config)
 
-    %%% Setup variables needed for snapshot saving %%%
+    %%% Setup parameters needed for snapshot saving %%%
 
-    steps = variables('t_i'):variables('dt'):variables('t_f');
+    steps = parameters('t_i'):parameters('dt'):parameters('t_f');
     
     Psi_snapshots = cell(1, steps);
     M_snapshots = cell(1, steps);
@@ -96,12 +93,12 @@ function [Psi_snapshots, M_snapshots] = simulate(Psi, M, variables, config)
             snapshot(Psi, M, t);
         end
         if config('print') == 1 && mod(i, printstep) == 0 % If enabled, print snapshots to .csv
-            fprintf("Simulation is %d%% done\n", ceil(100 * (t + variables('dt')) / variables('t_f')))
+            fprintf("Simulation is %d%% done\n", ceil(100 * (t + parameters('dt')) / parameters('t_f')))
         end
         Psi_snapshots{i} = Psi;
         M_snapshots{i} = M;
-        [Psi, M] = mapcell(Psi, M, variables('dt'), config); % Update cell columns and diffusion matrix using cellular model
-        M = M + Diffusion(M, variables('d'))*variables('dt');             % Call diffusion solver on M
+        [Psi, M] = mapcell(Psi, M, parameters('dt'), config); % Update cell columns and diffusion matrix using cellular model
+        M = M + Diffusion(M, parameters('d'))*parameters('dt');             % Call diffusion solver on M
         for col=1:size(Psi,2) % Send M Ao to Psi to ensure that diffusion changes are saved to cells
             column = Psi(:, col);
             Psi(config('Psi_Ao'), col) = M(column(config('Psi_x')), column(config('Psi_y')));
@@ -118,7 +115,7 @@ function [Psi, M] = mapcell(Psi, M, dt, config)
     if config('workers') > 0
         % Parallelized for loop of all independent cell model calculations
         parfor (col=1:size(Psi,2), config('workers'))
-            Psi(:, col) = Psi(:, col) + Function(Psi(:, col))*dt;
+            Psi(:, col) = Psi(:, col) + Cellular_Function(Psi(:, col))*dt;
         end
         % Update m_0's A_0 values for each occupied location. This must be
         % done outside of parfor as it would otherwise require passing in
@@ -132,7 +129,7 @@ function [Psi, M] = mapcell(Psi, M, dt, config)
         % For a nonparallel simulation we can just do all of this at once
         for col=1:size(Psi,2)
             column = Psi(:, col);
-            Psi(:, col) = column + Function(column)*dt;
+            Psi(:, col) = column + Cellular_Function(column)*dt;
             M(column(config('Psi_x')), column(config('Psi_y'))) = column(config('Psi_Ao'));
             %Psi
         end
