de7ccc2748edecf13b710f4249dc26275560baf0
DiskiNir <nid5xq@virginia.edu>
2018-07-31 12:44:52 -0400

    GridView Kinda working

--- GridView.m
+++ GridView.m
@@ -1,78 +1,98 @@
-function [ X ] = GridView(M,PSI,PSIIndex)
+function [ X ] = GridView(M,M_cells,Psi,Psi_cells,PSIIndex,ti,tf,n)
 %Creates a grid based on various elements of PSI or M
 %PSIIndex references ROW in PSI matrix
-close all
+close all;
+
+%Determine number of Cells
+PSISIZE =size(Psi);
+numofcells = PSISIZE(2);
+
+%Determine Dim of Media
+MSIZE =size(M);
+MH = MSIZE(1);
+MW = MSIZE(2);
 
 if PSIIndex == 0
     
-    %Determine Dim of Matrix M
-    MSIZE =size(M);
-    MH = MSIZE(1);
-    MW = MSIZE(2);
-    GRID = zeros(MH,MW);
-    
-    %Determine number of Cells
-    PSISIZE =size(PSI);
-    numofcells = PSISIZE(2);
-    
-    marksize = 6;
-    %Change value of grid to corresponding value in Matrix M.
-    for Mcounter = 1:MH*MW
-        GRID(Mcounter) = M(Mcounter);
-    end
-    
-    figure(1);
-    imagesc(GRID);
-    axis image;
-    colormap winter;
-    colorbar;
-    hold on;
-    
-    for Cellcounter = 1:numofcells
-        plot(PSI(1,Cellcounter),PSI(2,Cellcounter), 'o',...
-        'MarkerSize',marksize,...        
-        'MarkerEdgeColor','r');
-    end
-    
+    %     %Initalize MGRID
+    %     MGRID = cell(1,tf);
+    %     for counter = 1:length(MGRID)
+    %         MGRID{counter} = zeros(MH,MW);
+    %     end
 else
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    
-    %Determine number of Cells
-    PSISIZE =size(PSI);
-    numofcells = PSISIZE(2);
-    H = 100;
-    W = 100;
-    marksize = 3;
-    GRID = zeros(H,W);
-    BooleanGrid = zeros(H,W);
-    
-    %Change value of grid to corresponding value in Matrix M.
-    for Cellcounter = 1:numofcells
-        GRID(PSI(1,Cellcounter),PSI(2,Cellcounter)) = PSI(PSIIndex,Cellcounter);
-        BooleanGrid(PSI(1,Cellcounter),PSI(2,Cellcounter)) = 1;
+    %Intialize Cell GRID
+    cellGRID = cell(1,tf);
+    for counter = 1:length(cellGRID)
+        cellGRID{counter} = zeros(MH,MW);
     end
-    
-    figure(1);
-    imagesc(GRID);
-    axis image;
-    colormap winter;
-    colorbar;
-    hold on;
-    
-    for Hcounter = 1:H
-        for Wcounter = 1:W
-            if BooleanGrid(Hcounter,Wcounter) == 0
-                plot(Wcounter,Hcounter,'s',...
-                        'MarkerSize',marksize,...
-                        'MarkerEdgeColor','w',...
-                        'MarkerFaceColor',[1,1,1]);
+    %Intialize BooleanGrid
+    BooleanGrid = cellGRID;
+end
+
+f = figure('visible','off','position',...
+    [360 500 600 600]);
+
+slider =uicontrol('style','slider','position',[160 60 300 20],...
+    'min',ti,...
+    'max',tf,...
+    'SliderStep', [ (1/n) (2/n)],...
+    'Value',1,...
+    'callback',@callbackfn);
+
+hsttext=uicontrol('style','text',...
+    'position',[170 340 40 15],'visible','off');
+
+axes('units','pixels','position',[90 125 450 450]);
+
+movegui(f,'center');
+
+set(f,'visible','on');
+    function callbackfn(source,eventdata)
+        timestamp=round(get(slider,'value'));
+        
+        if PSIIndex ==0
+            %Determine size of the X's
+            marksize = 6;
+            
+            %Change value of grid to corresponding value in Matrix M.
+            imagesc(M_cells{timestamp});
+            %             axis image;
+            colormap winter;
+            colorbar;
+            hold on;
+            
+            for Cellcounter = 1:numofcells
+                plot(Psi_cells{timestamp}(1,Cellcounter),Psi_cells{timestamp}(2,Cellcounter), 'o',...
+                    'MarkerSize',marksize,...
+                    'MarkerEdgeColor','r');
+            end
+        else
+            marksize = 3;
+            
+            %Change value of grid at each timestamp to corresponding value in Matrix PSI
+            for CellNum = 1:numofcells
+                cellGRID{timestamp}(Psi_cells{timestamp}(1,CellNum),Psi_cells{timestamp}(2,CellNum)) = Psi_cells{timestamp}(PSIIndex,CellNum);
+                BooleanGrid{timestamp}(Psi_cells(1,CellNum),Psi_cells(2,CellNum)) = 1;
+            end
+            imagesc(cellGRID);
+            %axis image;
+            %imshow();
+            colormap winter;
+            colorbar;
+            hold on;
+            for Hcounter = 1:H
+                for Wcounter = 1:W
+                    if BooleanGrid{cellGRID}(Hcounter,Wcounter) == 0
+                        plot(Wcounter,Hcounter,'s',...
+                            'MarkerSize',marksize,...
+                            'MarkerEdgeColor','w',...
+                            'MarkerFaceColor',[1,1,1]);
+                    end
+                end
             end
         end
     end
-end
-
-% PSI = [ 10 35 59 79; 19 20 40 92; 4 7 9 2]
-% M = rand(100)*10;
 
 end
+
 
--- TEST.m
+++ TEST.m
@@ -1,21 +1,33 @@
-clc
-clear
-close all
+%Slider Test
+function [x] = TEST(M,M_cells,Psi,Psi_cells,PSIIndex,ti,tf,dt)
+close all;
+f = figure('visible','off','position',...
+    [360 500 600 600]);
 
-N = 10;
-M = 12;
+slider =uicontrol('style','slider','position',[160 60 300 20],...
+    'min',ti,...
+    'max',tf,...
+    'SliderStep', [ (dt/tf) (dt*2/tf)],...
+    'Value',1,...
+    'callback',@callbackfn);
 
-GRID = zeros(N,M);
+hsttext=uicontrol('style','text',...
+    'position',[170 340 40 15],'visible','off');
 
-Size =size(GRID);
+axes('units','pixels','position',[90 125 450 450]);
 
-i = 2;
-j= 5;
-GRID(i,j) = 5;
+movegui(f,'center');
 
-figure(1);
-imagesc(GRID);
-axis image;
-
-colormap winter;
-colorbar;
+set(f,'visible','on');
+    function callbackfn(source,eventdata)
+        num=get(slider,'value');
+        visual = imread(strcat('Matrix_at_TS_',num2str(num),'.png')); %%%Make for Cell_at_TS too
+        visual = imresize(visual, [450 450]);
+        imshow(visual);
+%         imagesc(GRID{num});
+%         axis image;
+%         colormap winter;
+%         colorbar;
+%         hold on;
+    end
+end
