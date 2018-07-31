17aa13d0e05aa4eac0128d73c86c3f6207dae3eb
DiskiNir <nid5xq@virginia.edu>
2018-07-31 16:41:09 -0400

    Fully Functional GridView

--- GridView.m
+++ GridView.m
@@ -12,35 +12,29 @@ MSIZE =size(M);
 MH = MSIZE(1);
 MW = MSIZE(2);
 
-if PSIIndex == 0
-    
-    %     %Initalize MGRID
-    %     MGRID = cell(1,tf);
-    %     for counter = 1:length(MGRID)
-    %         MGRID{counter} = zeros(MH,MW);
-    %     end
-else
+if PSIIndex ~= 0
     %Intialize Cell GRID
     cellGRID = cell(1,tf);
     for counter = 1:length(cellGRID)
         cellGRID{counter} = zeros(MH,MW);
     end
     %Intialize BooleanGrid
-    BooleanGrid = cellGRID;
+    BooleanGrid = zeros(MH,MW);
 end
 
 f = figure('visible','off','position',...
     [360 500 600 600]);
 
-slider =uicontrol('style','slider','position',[160 60 300 20],...
+slider =uicontrol('style','slider','position',[150 60 300 20],...
     'min',ti,...
     'max',tf,...
     'SliderStep', [ (1/n) (2/n)],...
     'Value',1,...
     'callback',@callbackfn);
 
-hsttext=uicontrol('style','text',...
-    'position',[170 340 40 15],'visible','off');
+text=uicontrol('style','text',...
+    'position',[200 30 200 15],'visible','on');
+
 
 axes('units','pixels','position',[90 125 450 450]);
 
@@ -49,44 +43,42 @@ movegui(f,'center');
 set(f,'visible','on');
     function callbackfn(source,eventdata)
         timestamp=round(get(slider,'value'));
-        
+        set(text,'String',strcat('Now Displaying #',num2str(timestamp)));
         if PSIIndex ==0
             %Determine size of the X's
-            marksize = 6;
+            marksize = 4.5;
             
             %Change value of grid to corresponding value in Matrix M.
             imagesc(M_cells{timestamp});
-            %             axis image;
+            axis image;
             colormap winter;
             colorbar;
             hold on;
             
             for Cellcounter = 1:numofcells
-                plot(Psi_cells{timestamp}(1,Cellcounter),Psi_cells{timestamp}(2,Cellcounter), 'o',...
+                plot(Psi_cells{timestamp}(1,Cellcounter),Psi_cells{timestamp}(2,Cellcounter), '-s',...
                     'MarkerSize',marksize,...
                     'MarkerEdgeColor','r');
             end
         else
-            marksize = 3;
+            marksize = 4.5;
             
             %Change value of grid at each timestamp to corresponding value in Matrix PSI
             for CellNum = 1:numofcells
                 cellGRID{timestamp}(Psi_cells{timestamp}(1,CellNum),Psi_cells{timestamp}(2,CellNum)) = Psi_cells{timestamp}(PSIIndex,CellNum);
-                BooleanGrid{timestamp}(Psi_cells(1,CellNum),Psi_cells(2,CellNum)) = 1;
+                BooleanGrid(Psi_cells{timestamp}(1,CellNum),Psi_cells{timestamp}(2,CellNum)) = 1;
             end
-            imagesc(cellGRID);
-            %axis image;
-            %imshow();
+            imagesc(cellGRID{timestamp});
+            axis image;
             colormap winter;
             colorbar;
             hold on;
-            for Hcounter = 1:H
-                for Wcounter = 1:W
-                    if BooleanGrid{cellGRID}(Hcounter,Wcounter) == 0
-                        plot(Wcounter,Hcounter,'s',...
+            for Hcounter = 1:MH
+                for Wcounter = 1:MW
+                    if BooleanGrid(Hcounter,Wcounter) == 1
+                        plot(Wcounter,Hcounter,'-s',...
                             'MarkerSize',marksize,...
-                            'MarkerEdgeColor','w',...
-                            'MarkerFaceColor',[1,1,1]);
+                             'MarkerEdgeColor','red');
                     end
                 end
             end
