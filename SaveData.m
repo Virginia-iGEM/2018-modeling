a6a468c94a8113a5b68a0b69f77debde61145e81
Mantissa-23 <dylanculfogienis@gmail.com>
2018-09-12 13:32:54 -0400

    SaveData is now a function

--- SaveData.m
+++ SaveData.m
@@ -1,15 +1,17 @@
-M_cells_json = jsonencode(M_cells);
-Psi_cells_json = jsonencode(Psi_cells);
+function SaveData(M_cells, Psi_cells)
+    M_cells_json = jsonencode(M_cells);
+    Psi_cells_json = jsonencode(Psi_cells);
 
-ds = datestr(now, 'dd-mmm-yyyy_HH-MM-SS');
+    ds = datestr(now, 'dd-mmm-yyyy_HH-MM-SS');
 
-mkdir('data');
-mkdir(['data/', ds]);
+    mkdir('data');
+    mkdir(['data/', ds]);
 
-M_file = fopen(['data/', ds, '/M_cells_', ds, '.json'], 'w');
-Psi_file = fopen(['data/', ds, '/Psi_cells_', ds, '.json'], 'w');
+    M_file = fopen(['data/', ds, '/M_cells_', ds, '.json'], 'w');
+    Psi_file = fopen(['data/', ds, '/Psi_cells_', ds, '.json'], 'w');
 
-fprintf(M_file, M_cells_json);  
-fprintf(Psi_file, Psi_cells_json);
+    fprintf(M_file, M_cells_json);  
+    fprintf(Psi_file, Psi_cells_json);
 
-save(['data/', ds, '/matlab_data_', ds], 'M_cells', 'Psi_cells');
+    save(['data/', ds, '/matlab_data_', ds], 'M_cells', 'Psi_cells');
+end
