048cee1e57cf9964e2a734c8a5aab085f4d56e86
rytaylor <rmt2dt@virginia.edu>
2018-09-19 15:03:56 -0400

    yeet

--- DefaultDONOTEDIT/Model_Population.m
+++ DefaultDONOTEDIT/Model_Population.m
@@ -21,14 +21,14 @@ import GridView.*
 
 %Initial Parameters
 para = containers.Map;
-para('n') = 4;                      % Number of Cells (needs to be square number)
-para('m') = 25;                     % Number of Parameters for each Cell
-para('w') = 2*round(para('n')^(3/4));           % Medium/Diffusion Grid Width
-para('h') = 2*round(para('n')^(3/4));           % Medium/Diffusion Grid height
+para('n') = 8^2;      %DEFAULT = 64         % Number of Cells (needs to be square number)
+para('m') = 25;                             % Number of Parameters for each Cell
+para('w') = ceil(para('n')^(3/4));           % Medium/Diffusion Grid Width
+para('h') = ceil(para('n')^(3/4));           % Medium/Diffusion Grid height
 para('t_i') = 0;           %DEFAULT = 0         % Set initial time to 0
 para('t_f') =  100;         %DEFAULT = 100       % Final time
-para('dt')= 10^(-3);       %DEFAULT = 10^(-5)   % Constant timestep 
-para('D') = 10^2;          %DEFAULT = 10^(5)    % Diffusion coefficient
+para('dt')= 10^(-3);       %DEFAULT = 10^(-3)   % Constant timestep 
+para('D') = 10^3;          %DEFAULT = 10^(3)    % Diffusion coefficient
 parmeters('index') = 0;
 %--------------------------------------------------------
 
@@ -152,8 +152,8 @@ end
 for i = 1:round(sqrt(para('n')))
     for j = 1:round(sqrt(para('n')))
         if iter<=para('n')
-            Psi(1,iter) = round(para('w')/2-sqrt(para('n')/2)+2*(i-1));
-            Psi(2,iter) = round(para('h')/2-sqrt(para('n')/2))+2*(j-1);
+            Psi(1,iter) = round(para('w')/2-sqrt(para('n')/2)+2*(i-1))-1;
+            Psi(2,iter) = round(para('h')/2-sqrt(para('n')/2))+2*(j-1)-1;
             iter = iter+1;
         end
     end
@@ -181,3 +181,4 @@ end
 %Simulate
 [Psi_cells, M_cells,time] = Structure(Psi, M, para, config);
 %-----------------
+SaveData(M_cells,Psi_cells,time);
--- Rivanna4/Analyze_Model.m
+++ Rivanna4/Analyze_Model.m
@@ -1,5 +1,5 @@
 clear Figure1
-var_display = 'Ao';
+var_display = 'Ap';
 %Statistically Analyze
 %Graph
 Readout1 = zeros(para('n'),config('n_snapshots'));
@@ -21,5 +21,5 @@ for i=1:para('n')
     legend(var_display)
 end
 hold off
-GridView(M_cells,Psi_cells,0*var(var_display),para('t_i'),para('t_f'),config('n_snapshots'));
+GridView(M_cells,Psi_cells,var(var_display),para('t_i'),para('t_f'),config('n_snapshots'));
 %SaveData(M_cells, Psi_cells, time)
