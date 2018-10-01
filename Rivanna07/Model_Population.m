0cf29d886440da7eaae871027f5b9f3856cf2bf2
rytaylor <rmt2dt@virginia.edu>
2018-10-01 19:15:18 -0400

    prep for ST10-1

--- Rivanna07/Model_Population.m
+++ Rivanna07/Model_Population.m
@@ -116,7 +116,7 @@ c_i(var('Y_p|mrna')) =    0;
 c_i(var('X_g')) =       5.85966;
 c_i(var('X_p')) =     0;
 c_i(var('X_p|mrna'))= 0;
-c_i(var('Y_g')) =     1.4565;
+c_i(var('Y_g')) =     1.4565*10;
 c_i(var('Y_p')) =     0;
 c_i(var('Y_p|mrna'))= 0;
 %--------------------------
