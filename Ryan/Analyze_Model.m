43d75c42c980768ef09c08be0aae079f21fdac9f
rytaylor <rmt2dt@virginia.edu>
2018-10-04 16:50:55 -0400

    analyses done

--- Ryan/Analyze_Model.m
+++ Ryan/Analyze_Model.m
@@ -1,7 +1,7 @@
 clear all;
 
-CORE = '19'; %Which Rivanna core do you want to run
-runfeature = 'kXS/100';       %What changes are being tested
+CORE = '21'; %Which Rivanna core do you want to run
+runfeature = 'Ki/100\_Kimrna/100';       %What changes are being tested
 var_display = {'Ap','Ai','Ao','R','K','B','T','G',};   %What variables to display
 save = false;
 
