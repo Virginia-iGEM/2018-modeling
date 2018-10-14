9edbf94382e5ec17fcdd709ae4e1aa1e8a1046d3
rytaylor <rmt2dt@virginia.edu>
2018-10-14 08:08:50 -0400

    ASFDASFASDD

--- Ryan/Analyze_Model.m
+++ Ryan/Analyze_Model.m
@@ -1,6 +1,6 @@
 clear all;
 
-CORE = '02'; %Which Rivanna core do you want to run
+CORE = '01'; %Which Rivanna core do you want to run
 runfeature = 'rRR*7\_N(10-14)';       %What changes are being tested
 var_display = {'Ap','Ai','Ao','R','K','B','T','G','F'};   %What variables to display
 save = false;
